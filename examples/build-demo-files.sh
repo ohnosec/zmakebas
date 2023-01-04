85e3823b0cedb255f54c9f47e254a99bf1b74ffe
Chris Young <chris@unsatisfactorysoftware.co.uk>
2023-01-04 14:02:33 +0000

    Build ZX Next demo in +3 mode, can be copied and run direct from SD card.

--- examples/build-demo-files.sh
+++ examples/build-demo-files.sh
@@ -6,11 +6,10 @@
 ../zmakebas -i 10 -a 10 -l -o demolbl.tap -n "demolbl" demolbl.bas
 
 # ZX Spectrum Next
-../zmakebas -a 10 -o zx-next-demo.tap -n "nextdemo" zx-next-demo.bas
-../zmakebas -i 10 -a 10 -l -o zx-next-demolbl.tap -n "nextdemolb" zx-next-demolbl.bas
+../zmakebas -a 10 -3 -o zxn-demo.bas zx-next-demo.bas
+../zmakebas -i 10 -a 10 -3 -l -o zxn-demolbl.bas -n "nextdemolb" zx-next-demolbl.bas
 
 # ZX81
 ../zmakebas -a 10 -p -o zx81-basic-demo.p zx81-basic-demo.bas
 ../zmakebas -i 10 -a 10 -l -p -o zx81-basic-demo-lbl.p zx81-basic-demo-lbl.bas
 
-exit
